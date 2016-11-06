6b55a2615bcae3c6c46ea1ffbf55492ae4776967
saraducks <sara@vijays-MacBook-Pro.local>
2016-11-06 11:52:53 -0800

    added user login

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID ='954073853990-5g0s539rdfrq15fo5iiv5c1cq7ch56ra.apps.googleusercontent.com'
+
